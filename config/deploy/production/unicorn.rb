b950da3a545229e4ad8bfaea8f4cbe630a75281c
jason <jason.addie@gmail.com>
2017-12-11 10:25:12 +0400

    setting prod deploy

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,8 +1,8 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/xtraktr/Xtraktr/current" # path to application current folder
-sock_name = "unicorn_Xtraktr" # must be unique name with no '.'
+root = "/home/deploy/Flex/current" # path to application current folder
+sock_name = "unicorn_Flex" # must be unique name with no '.'
 port_num = 8148 # must be a unique port number for this application
 tout = 1800 # time in seconds before process dies - need a long time for data uploads
 ##################################
