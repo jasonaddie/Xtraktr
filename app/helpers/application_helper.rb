4a4657250aec4c971a401c8ebed9997765aacd09
Jason <jason.addie@gmail.com>
2019-04-04 15:32:43 +0200

    bug fix - to_str

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,7 +31,7 @@ module ApplicationHelper
 		text = '' if text.nil?
 		text = smart_truncate(text, options[:truncate]) if options[:truncate].present?
 		text = sanitize(text) unless options[:sanitize] == false
-		text = text.to_str
+		text = text.to_s
 		text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
 #		text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
 		text.html_safe
