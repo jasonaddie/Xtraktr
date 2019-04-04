f112a489f6e2fb72d23c14fc7e8804cbea3d2757
Jason <jason.addie@gmail.com>
2019-04-04 15:08:23 +0200

    change deploy for forset server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-server "data.unicef.ge", :web, :app, :db, primary: true # server where app is located
-set :application, "Data-Portal" # unique name of application
+server "unicef.forset.ge", :web, :app, :db, primary: true # server where app is located
+set :application, "Unicef-Data-Portal" # unique name of application
 set :user, "deploy"# name of user on server
 set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "production/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "ForSetGeorgia" # name of accout on git hub
 set :github_repo_name, "Xtraktr" # name of git hub repo
-set :git_branch_name, "unicef-master" # name of branch to deploy
+set :git_branch_name, "unicef-server-test" # name of branch to deploy
 set :rails_env, "production" # name of environment: production, staging, ...
 ##################################
--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,8 +1,8 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/deploy/Data-Portal/current" # path to application current folder
-sock_name = "unicorn_Data-Portal" # must be unique name with no '.'
+root = "/home/deploy/Unicef-Data-Portal/current" # path to application current folder
+sock_name = "unicorn_Unicef-Data-Portal" # must be unique name with no '.'
 port_num = 8140 # must be a unique port number for this application
 tout = 1200 # time in seconds before process dies - need a long time for data uploads
 ##################################
@@ -14,5 +14,5 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/#{sock_name}.sock"
 listen port_num, :tcp_nopush => true
-worker_processes 4
+worker_processes 1
 timeout tout
