5ff2eaae13359693afd98d9fc6ed1a075874d616
Jason <jason.addie@gmail.com>
2019-08-22 23:20:26 +0200

    update deploy github account

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :application, "Unicef-Data-Portal" # unique name of application
 set :user, "deploy"# name of user on server
 set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "production/unicorn_init.sh" # location of unicor init shell file
-set :github_account_name, "ForSetGeorgia" # name of accout on git hub
+set :github_account_name, "jasonaddie" # name of accout on git hub
 set :github_repo_name, "Xtraktr" # name of git hub repo
 set :git_branch_name, "unicef-master" # name of branch to deploy
 set :rails_env, "production" # name of environment: production, staging, ...
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,12 +1,12 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-server "alpha.jumpstart.ge", :web, :app, :db, primary: true # server where app is located
-set :application, "Unicef-Staging" # unique name of application
-set :user, "unicef-staging" # name of user on server
+server "dev-unicef.forset.ge", :web, :app, :db, primary: true # server where app is located
+set :application, "Unicef-Data-Portal" # unique name of application
+set :user, "deploy"# name of user on server
 set :ngnix_conf_file_loc, "staging/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "staging/unicorn_init.sh" # location of unicor init shell file
-set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
+set :github_account_name, "jasonaddie" # name of accout on git hub
 set :github_repo_name, "Xtraktr" # name of git hub repo
 set :git_branch_name, "unicef-master" # name of branch to deploy
 set :rails_env, "staging" # name of environment: production, staging, ...
