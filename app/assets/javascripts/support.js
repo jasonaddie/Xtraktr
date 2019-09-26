640fe7e769635463afe3ffa165d6a1fc3c8d4ed2
Jason <jason.addie@gmail.com>
2019-09-26 20:34:15 +0200

    add h3 headers to sidebar

--- app/assets/javascripts/support.js
+++ app/assets/javascripts/support.js
@@ -4,16 +4,31 @@ $(function() {
   anchors.add('.support-col-left h2, .support-col-left h3, .support-col-left h4, .support-col-left h5, .support-col-left h6');
 
   // pull all of the headers out of the content and build a table of contents with it
-  var $headers = $('.support-col-left h2')
+  var $headers = $('.support-col-left h2, .support-col-left h3')
   var $in_guide = $('.support-col-right .in-this-guide')
   var $ul = $('.support-col-right ul')
-    console.log($headers)
+  var last_header = null
+  var html = ''
+
   if ($headers.length > 0){
     $headers.each(function(){
-      $ul.append('<li><a href="#' + $(this).attr('id') + '">' + $(this).text() + '</a></li>')
+      if ($(this).text().trim() !== ''){
+        if ($(this)[0].nodeName == 'H2' && last_header === 'H3'){
+          html += '</ul>'
+        }else if ($(this)[0].nodeName == 'H3' && last_header === 'H2'){
+          html += '<ul>'
+        }
+        html += '</li>'
+        html += '<li><a href="#' + $(this).attr('id') + '">' + $(this).text() + '</a>'
+        last_header = $(this)[0].nodeName
+      }
     })
+    // close the last list item
+    if (html.length > 0){
+      html += '</li>'
+    }
+    $ul.append(html)
   }else{
-    console.log('no headers')
     $in_guide.addClass('no-content')
   }
 
