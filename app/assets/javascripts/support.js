9d5a3adb34b754d9fb37f6c631c011f437e7d9ee
Jason <jason.addie@gmail.com>
2019-09-26 17:35:49 +0200

    add guide last updated date

--- app/assets/javascripts/support.js
+++ app/assets/javascripts/support.js
@@ -5,11 +5,16 @@ $(function() {
 
   // pull all of the headers out of the content and build a table of contents with it
   var $headers = $('.support-col-left h2')
+  var $in_guide = $('.support-col-right .in-this-guide')
   var $ul = $('.support-col-right ul')
+    console.log($headers)
   if ($headers.length > 0){
     $headers.each(function(){
       $ul.append('<li><a href="#' + $(this).attr('id') + '">' + $(this).text() + '</a></li>')
     })
+  }else{
+    console.log('no headers')
+    $in_guide.addClass('no-content')
   }
 
 
